1201b85abaeca32453e47bbcf9ec2d73699fcd3b
DaisySSLMLK <127324228+DaisySSLMLK@users.noreply.github.com>
2023-07-08 17:25:03 +0200

    Updated the js event

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 function showSelectedTimezone(event) {
-  if (event.target.value.length > 1) {
-    if (event.target.value === "paris") {
-      alert(`It is currently ${parisTime} in Europe/Paris`);
-    }
-    if (event.target.value === "sydney") {
-      alert(`It is currently ${sydneyTime} in Australia/Sydney`);
-    }
-    if (event.target.value === "tokyo") {
-      alert(`It is currently ${tokyoTime} in Asia/Tokyo`);
-    }
+  if (event.target.value.length > 0) {
+    let currentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY HH:mm");
+
+    alert(`It is ${currentTime} in ${event.target.value}`);
   }
 }
 let timezoneSelect = document.querySelector("#country");
 timezoneSelect.addEventListener("change", showSelectedTimezone);
-
-let parisTime = moment().tz("Europe/Paris").format("dddd, MMMM D, YYYY HH:mm");
-let sydneyTime = moment()
-  .tz("Australia/Sydney")
-  .format("dddd, MMMM D, YYYY HH:mm");
-let tokyoTime = moment().tz("Asia/Tokyo").format("dddd, MMMM D, YYYY HH:mm");
